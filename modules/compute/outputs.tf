746d440b818fa26e6dfd31f0b6c20412e4130700
Henrique Mozart de Souza <henrique.mozart@yahoo.com.br>
2026-04-03 15:54:34 -0300

    refactor: fix loop name

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,1 +1,4 @@
-output "public_ip" { value = aws_instance.app_server.public_ip }
+output "cluster_name" {
+  description = "O nome do cluster ECS criado"
+  value       = aws_ecs_cluster.main.name
+}
